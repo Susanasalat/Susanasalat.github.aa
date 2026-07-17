3910d9420bbdb585e9fac258093376ee1faea77c
Susanasalat <susan.asaalat@dcmail.ca>
2026-07-17 11:03:23 -0400

    Complete Part 2 JavaScript

---
+++ Assignment4/Part2/main.js
@@ -0,0 +1,45 @@
+const displayedImage = document.querySelector(".displayed-img");
+const thumbBar = document.querySelector(".thumb-bar");
+
+const btn = document.querySelector("button");
+const overlay = document.querySelector(".overlay");
+
+
+const images = [
+  { filename: "pic1.jpg", alt: "Closeup of a human eye" },
+  { filename: "pic2.jpg", alt: "Rock that looks like a wave" },
+  { filename: "pic3.jpg", alt: "Purple and white pansies" },
+  { filename: "pic4.jpg", alt: "Section of wall from a pharaoh's tomb" },
+  { filename: "pic5.jpg", alt: "Large moth on a leaf" },
+];
+
+const imagePath =
+  "https://mdn.github.io/shared-assets/images/examples/learn/gallery/";
+
+for (const image of images) {
+  const newImage = document.createElement("img");
+
+  newImage.setAttribute("src", `${imagePath}${image.filename}`);
+  newImage.setAttribute("alt", image.alt);
+
+  newImage.addEventListener("click", () => {
+  displayedImage.src = newImage.src;
+  displayedImage.alt = newImage.alt;
+});
+
+  thumbBar.appendChild(newImage);
+}
+
+btn.addEventListener("click", () => {
+  const btnClass = btn.getAttribute("class");
+
+  if (btnClass === "dark") {
+    btn.setAttribute("class", "light");
+    btn.textContent = "Lighten";
+    overlay.style.backgroundColor = "rgb(0 0 0 / 0.5)";
+  } else {
+    btn.setAttribute("class", "dark");
+    btn.textContent = "Darken";
+    overlay.style.backgroundColor = "rgb(0 0 0 / 0)";
+  }
+});
