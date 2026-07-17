c6c45433f713fd701b6a14fc59ee9d2c0e20a6c9
Susanasalat <susan.asaalat@dcmail.ca>
2026-07-17 12:03:21 -0400

    Complete Part 4 evil circle

--- Assignment4/Part4/main.js
+++ Assignment4/Part4/main.js
@@ -71,6 +71,70 @@ class Ball extends Shape {
     }
   }
 }
+class EvilCircle extends Shape {
+  constructor(x, y) {
+    super(x, y, 20, 20);
+    this.color = "white";
+    this.size = 10;
+
+    window.addEventListener("keydown", (e) => {
+      switch (e.key) {
+        case "a":
+          this.x -= this.velX;
+          break;
+        case "d":
+          this.x += this.velX;
+          break;
+        case "w":
+          this.y -= this.velY;
+          break;
+        case "s":
+          this.y += this.velY;
+          break;
+      }
+    });
+  }
+
+  draw() {
+    ctx.beginPath();
+    ctx.lineWidth = 3;
+    ctx.strokeStyle = this.color;
+    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+    ctx.stroke();
+  }
+
+  checkBounds() {
+    if (this.x + this.size >= width) {
+      this.x = width - this.size;
+    }
+
+    if (this.x - this.size <= 0) {
+      this.x = this.size;
+    }
+
+    if (this.y + this.size >= height) {
+      this.y = height - this.size;
+    }
+
+    if (this.y - this.size <= 0) {
+      this.y = this.size;
+    }
+  }
+
+  collisionDetect() {
+    for (const ball of balls) {
+      if (ball.exists) {
+        const dx = this.x - ball.x;
+        const dy = this.y - ball.y;
+        const distance = Math.sqrt(dx * dx + dy * dy);
+
+        if (distance < this.size + ball.size) {
+          ball.exists = false;
+        }
+      }
+    }
+  }
+}
 
 const balls = [];
 
@@ -89,15 +153,31 @@ while (balls.length < 25) {
   balls.push(ball);
 }
 
+const ballCount = document.querySelector(".ball-count");
+
+const evilBall = new EvilCircle(
+  width / 2,
+  height / 2
+);
+
 function loop() {
   ctx.fillStyle = "rgb(0 0 0 / 0.25)";
   ctx.fillRect(0, 0, width, height);
 
   for (const ball of balls) {
-    ball.draw();
-    ball.update();
-    ball.collisionDetect();
+    if (ball.exists) {
+      ball.draw();
+      ball.update();
+      ball.collisionDetect();
+    }
   }
+
+  evilBall.draw();
+  evilBall.checkBounds();
+  evilBall.collisionDetect();
+
+  const remainingBalls = balls.filter((ball) => ball.exists).length;
+  ballCount.textContent = `Ball count: ${remainingBalls}`;
 
   requestAnimationFrame(loop);
 }
